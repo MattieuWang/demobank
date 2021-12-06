1651ca84d036e35d21d272dd91a8cd3d97ee2c72
Junzhe <mattieu.wang@particeep.com>
2021-12-06 22:41:02 +0100

    remove lines

--- src/test/java/com/junzhe/demobank/AuthControllerIntegrationTests.java
+++ src/test/java/com/junzhe/demobank/AuthControllerIntegrationTests.java
@@ -2,12 +2,7 @@ package com.junzhe.demobank;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectWriter;
-import com.junzhe.demobank.models.user.JwtUser;
 import com.junzhe.demobank.models.user.UserPayload;
-import com.junzhe.demobank.utils.CookieUtils;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -15,11 +10,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
-
 import javax.servlet.http.Cookie;
-
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.cookie;
